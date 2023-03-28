d506bc80e23c77e8e0da5b26b51793bbae865f6d
Brad Brown <bradbrown@bradbrown.llc>
2023-03-28 14:28:44 -0400

    header (holds logo, hamburger, title)

---
+++ UI/src/components/Header.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { P } from 'flowbite-svelte'
+    import Logo from './Logo.svelte'
+    import Hamburger from './Hamburger.svelte'
+    export let activeApp
+</script>
+
+<div class="grid grid-cols-3 py-1 px-2">
+    <Hamburger bind:activeApp/>
+    <div class="w-full h-[38px] col-start-2">
+        <Logo/>
+    </div>
+    <P class="px-2 lowercase flex flex-row-reverse items-center !font-bold !text-2xl text-indigo-900 dark:text-indigo-500">
+        {activeApp}
+    </P>
+</div>
