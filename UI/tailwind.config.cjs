214c4fbd35eb83bcbd05dda60846a5a92c281451
Brad Brown <bradbrown@bradbrown.llc>
2023-03-28 14:29:23 -0400

    boilerplate

---
+++ UI/tailwind.config.cjs
@@ -0,0 +1,11 @@
+const config = {
+    content: [
+        "./src/**/*.{html,js,svelte,ts}",
+        "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}",
+    ],
+    plugins: [
+        require('flowbite/plugin')
+    ],
+    darkMode: 'media'
+}
+module.exports = config
