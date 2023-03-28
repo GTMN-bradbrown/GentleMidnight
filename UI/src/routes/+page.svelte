275e192448f53dcc88e149b6f39542b1775358c6
Brad Brown <bradbrown@bradbrown.llc>
2023-03-28 14:31:12 -0400

    default page

---
+++ UI/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import Header from '../components/Header.svelte'
+    import Body from '../components/Body.svelte'
+    import appNames from '../json/appNames.json'
+    let activeApp = appNames[0]
+</script>
+
+<Header bind:activeApp/>
+<Body bind:activeApp/>
