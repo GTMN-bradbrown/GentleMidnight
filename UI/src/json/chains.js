6d2fe245bbb5f695563b60c09e524ed98c06d9d9
Brad Brown <bradbrown@bradbrown.llc>
2023-04-02 20:37:04 -0400

    chains json add

---
+++ UI/src/json/chains.js
@@ -0,0 +1,6 @@
+export default [
+    { name: "ETH", value: BigInt('0xaa36a7') },
+    { name: "FTM", value: BigInt('0xfa2') },
+    { name: "AVAX", value: BigInt('0xa869') },
+    { name: "BSC", value: BigInt('0x61') }
+]
