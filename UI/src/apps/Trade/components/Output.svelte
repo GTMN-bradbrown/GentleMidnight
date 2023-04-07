e04585940324239d5fbf5b7d49fd29b189545779
Brad Brown <bradbrown@bradbrown.llc>
2023-04-06 23:45:32 -0400

    making sure the branch thing works like i think it does

--- UI/src/apps/Trade/components/Output.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import Squeezy from './Squeezy.svelte'
-    export let subreq
-    let value = subreq.output
-    $: subreq.output = value, 
-        subreq.valid = !Object.values(subreq).includes(undefined)
-</script>
-
-<Squeezy label="Output" bind:value/>
