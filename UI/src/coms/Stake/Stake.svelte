1eedb65a3d41302eaafee8034bdfc258b7842188
Brad Brown <bradbrown@bradbrown.llc>
2023-04-10 16:40:17 -0400

    https://gitlab.com/bradbrown1/gentlemidnight/-/issues/8

--- UI/src/coms/Stake/Stake.svelte
+++ UI/src/coms/Stake/Stake.svelte
@@ -2,15 +2,22 @@
     import { Web3Input } from '@/coms/common'
     import { onMount, onDestroy } from 'svelte'
     import { medium } from '@/medium'
-    onMount(() => {
-        // ethereum.on('chainChanged', chainId => {
-        //     medium.unwatch({ app: 'stake', chainId})
-        // })
-        medium.watch({ app: 'stake', chainId: ethereum.chainId })
-    })
-    onDestroy(() => medium.unwatch({ app: 'stake', chainId: ethereum.chainId }))
-    let value
+    let value, chainId, app = 'stake'
     $: console.log(value)
+    onMount(() => {
+        chainId = ethereum.chainId
+        medium.watch({ app, chainId })
+        ethereum.on('chainChanged', onChainChanged)
+    })
+    onDestroy(() => {
+        medium.unwatch({ app, chainId })
+        ethereum.removeListener('chainChanged', onChainChanged)
+    })
+    function onChainChanged() {
+        medium.unwatch({ app, chainId })
+        chainId = ethereum.chainId
+        medium.watch({ app, chainId })
+    }
 </script>
 
 <Web3Input bind:value decimals={6} max={100000000000n}/>
