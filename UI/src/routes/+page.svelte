7b8438eb25b02f9f74b4121f046441ccffe5fbe0
Brad Brown <bradbrown@bradbrown.llc>
2023-04-10 15:53:21 -0400

    reorg page route, remove junk

--- UI/src/routes/+page.svelte
+++ UI/src/routes/+page.svelte
@@ -1,55 +1,18 @@
 <script>
-    import { Header } from '@/coms/Header'
-    import AppSelect from '../coms/AppSelect/AppSelect.svelte'
+    import { AppSelect, AppsButton } from '@/coms/AppSelect'
     import { App } from '@/coms/App'
 </script>
 
-<AppSelect/>
-<Header/>
-<App/>
-
-<!-- <script>
-    let apps = ['TRADE', 'MINE', 'STATS', 'HOLD', 'DUST', 'STAKE', 'CLAIM']
-</script>
-
 <div>
-    {#each apps as app}
-        <button>{app}</button>
-    {/each}
+    <AppSelect/>
+    <AppsButton/>
+    <App/>
 </div>
 
 <style>
     div {
         display: flex;
-        flex-wrap: wrap;
-        justify-content: center;
-        padding: 4px;
-        gap: 4px;
+        flex-direction: column;
+        align-items: center;
     }
-    button {
-        display: block;
-        width: 80px;
-        height: 40px;
-        background-color: transparent;
-        color: #3F00FF;
-        border: none;
-        cursor: pointer;
-        transition: all 0.125s ease;
-        border-radius: 15px;
-        font-family: 'Montserrat', sans-serif;
-        font-size: large;
-        border: 2px solid #3F00FF;
-    }
-    button:hover {
-        border-radius: 10px;
-    }
-    button:active {
-        border-radius: 5px;
-    }
-    @media (prefers-color-scheme: dark) {
-        button {
-            color: #F6F1D5;
-            border: 2px solid #F6F1D5;
-        }
-    }
-</style> -->
+</style>
