582fa3d1aa54d98b8af7bbca32b1f30dcf351c8c
Brad Brown <bradbrown@bradbrown.llc>
2023-04-10 15:54:34 -0400

    stake watches metamask selected chain on mount, unwatches on destroy

--- UI/src/coms/Stake/Stake.svelte
+++ UI/src/coms/Stake/Stake.svelte
@@ -1,5 +1,14 @@
 <script>
     import { Web3Input } from '@/coms/common'
+    import { onMount, onDestroy } from 'svelte'
+    import { medium } from '@/medium'
+    onMount(() => {
+        // ethereum.on('chainChanged', chainId => {
+        //     medium.unwatch({ app: 'stake', chainId})
+        // })
+        medium.watch({ app: 'stake', chainId: ethereum.chainId })
+    })
+    onDestroy(() => medium.unwatch({ app: 'stake', chainId: ethereum.chainId }))
     let value
     $: console.log(value)
 </script>
