ee649dafccf221284c3dc621239c74021db1507b
Brad Brown <bradbrown@bradbrown.llc>
2023-04-11 16:39:31 -0400

    multichain selector

--- UI/src/coms/Mine/Mine.svelte
+++ UI/src/coms/Mine/Mine.svelte
@@ -1,1 +1,20 @@
-mine
+<script>
+    import gtmn from '@/data/gtmn.js'
+    let chains = Object.values(gtmn).sort()
+    let value = []
+    $: console.log(value)
+</script>
+
+<select multiple bind:value size=3>
+    {#each chains as chain}
+        <option value={chain.name}>{chain.name}</option>
+    {/each}
+</select>
+
+<style>
+    option {
+        font-size: 125%;
+        padding: 2.5mm 1rem;
+        font-family: monospace;
+    }
+</style>
