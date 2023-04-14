99677c2ba73f6e583b65ee042e7f34bbbcb37132
Brad Brown <bradbrown@bradbrown.llc>
2023-04-13 21:36:23 -0400

    mine app with ugly comments before componentizing

--- UI/src/coms/Mine/Mine.svelte
+++ UI/src/coms/Mine/Mine.svelte
@@ -1,20 +1,55 @@
 <script>
-    import gtmn from '@/data/gtmn.js'
-    let chains = Object.values(gtmn).sort()
-    let value = []
-    $: console.log(value)
+    import { Selector } from './index.js'
+    // import { medium } from '@/medium'
+    // import gtmn from '@/data/gtmn.js'
+    // let app = 'mine'
+    // let chains = Object.entries(gtmn)
+    //     .sort((a, b) => a[1].name < b[1].name ? -1 : 1)
+    // let value = []
+    // let prevValue = []
+    // function onChange() {
+    //     let watch = value.filter(chain => !prevValue.includes(chain))
+    //     let unwatch = prevValue.filter(chain => !value.includes(chain))
+    //     for (let chainId of watch) medium.watch({ app, chainId })
+    //     for (let chainId of unwatch) medium.unwatch({ app, chainId })
+    //     prevValue = [...value]
+    // }
 </script>
 
-<select multiple bind:value size=3>
-    {#each chains as chain}
-        <option value={chain.name}>{chain.name}</option>
-    {/each}
-</select>
+<Selector/>
+
+<!-- <div class="foo">
+    <select multiple bind:value on:change={onChange}>
+        {#each chains as chain}
+            <option value={chain[0]}>
+                {chain[1].name}
+            </option>
+        {/each}
+    </select>
+    <div class="baz">
+        {#each chains as chain, i}
+            <div class="bar">
+                <div class="faz">block</div>
+                <div class="boo">28911930</div>
+                <div class="faz">int</div>
+                <div class="boo">5000</div>
+                <div class="faz">open</div>
+                <div class="boo">5</div>
+                <div class="faz">exc</div>
+                <div class="boo">1</div>
+            </div>
+        {/each}
+    </div>
+</div>
 
 <style>
-    option {
-        font-size: 125%;
-        padding: 2.5mm 1rem;
+    .boo {
+        color: #3F00FF;
+        font-size: 80%;
+        font-weight: 600;
         font-family: monospace;
+        display: flex;
+        justify-content: left;
+        align-items: center;
     }
-</style>
+</style> -->
